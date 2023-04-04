7f24ca3797445a9f5b62756109dbd5a3cd9c3722
Viktoriia <volshevska@luxoft.com>
2023-04-04 14:22:50 +0200

    comment adjusted

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -13,15 +13,21 @@ public class Main {
         int d = 3;
         int sum1 = a + b;
         int sum2 = c + d;
-        String result = sum1 > sum2 ? "True" : "False";
-        System.out.println(result);
+        /*String result = sum1 > sum2 ? "True" : "False";
+        System.out.println(result); first method*/
+
+        System.out.println(sum1 > sum2);//second
+
+        boolean result1= sum1 > sum2;
+        System.out.println(result1);//third
 
         sum1 = sum1 + 1;
         sum2 = sum2 - 2;
-        String result2 = sum1 > sum2 ? "True" : "False";
-        System.out.println(result2);
+        /*String result2 = sum1 > sum2 ? "True" : "False";
+        System.out.println(result2);*/
+        System.out.println(sum1 > sum2);
 
-        String result3 = sum1 % 2 == 0 || sum2 % 2 == 0 ? "True" : "False";
-        System.out.println(result3);
+        /*String result3 = sum1 % 2 == 0 || sum2 % 2 == 0 ? "True" : "False";*/
+        System.out.println(sum1 % 2 == 0 || sum2 % 2 == 0);
     }
 }
